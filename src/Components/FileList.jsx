c31281f77c7b5821e90b3707b55fc9463649933a
Alexander Makarof <makarofalex@gmail.com>
2020-03-11 15:37:38 +0300

    finished couch proj

--- src/Components/FileList.jsx
+++ src/Components/FileList.jsx
@@ -13,6 +13,37 @@ export default function FileList({ files }) {
           updated_at={updated_at}
         />
       ))}
+      <>
+        <IconButton>Awesome</IconButton>
+        <Alert title="HEY!">VERY IMPORTANT!</Alert>
+      </>
     </ul>
   );
 }
+
+export function IconButton({ children }) {
+  return (
+    <button type="button">
+      <i className="fas fa-cloud" />
+      {children}
+    </button>
+  );
+}
+
+export const Alert = ({ children, title }) => (
+  <div
+    style={{
+      backgroundColor: "#FFCCCB  ",
+      width: "300px",
+      color: "#CC0000  ",
+      minHeight: " 50px",
+      lineHeight: "50px",
+      textAlign: "center",
+      border: "1px solid red"
+    }}
+  >
+    <h3>{title}</h3>
+    <i className="fas fa-exclamation-triangle" style={{ marginLeft: "10px" }} />
+    {children}
+  </div>
+);
